e1c77d566fa3732c8f56cdab7e19be5db136027c
NastyaZiss <kurbatova.vladuslava050@gmail.com>
2022-11-12 17:26:38 +0300

    update PZ_5

--- PZ-5/PZ_5_1.py
+++ PZ-5/PZ_5_1.py
@@ -1,11 +1,14 @@
 #Составить функцию, которая напечатает сорок любых символов
 
-def SortInc3(num):
-    list_si = []
-    for i in range(60, 60 + num):
-            list_si.append(chr(i))
+sim = input("Введите символ: ")
 
-    return ''.join(list_si)
+def SortInc3(sim):
+    return sim*40
+    # list_si = []
+    # for i in range(60, 60 + num):
+    #         list_si.append(chr(i))
+    #
+    # return ''.join(list_si)
 
 print("40 любых символов: ", end=" ")
-print(SortInc3(40))
+print(SortInc3(sim))
