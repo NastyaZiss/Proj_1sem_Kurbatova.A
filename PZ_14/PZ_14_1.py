92bc789478f2fc4feef9fe2efe942cb1776056d7
NastyaZiss <kurbatova.vladuslava050@gmail.com>
2023-03-04 15:22:17 +0300

    The PZ_14 ready

--- PZ_14/PZ_14_1.py
+++ PZ_14/PZ_14_1.py
@@ -1,15 +1,11 @@
-import re
 # Из исходного текстового файла (Dostoevsky.txt) выбрать блок информации за 1857
 # год и поместить ее в новый текстовый файл.
 
 file = 'Dostoevsky.txt'
 import re
 
-
-
 with open(file, 'r', encoding='utf-8') as file:
  text = file.read()
- p = re.compile(r"После.*",re.M)
+#  print(text)
+ p = re.compile(r"После .*",re.M)
  print(p.findall(text))
- #reg_name = re.split(p, text)
-
